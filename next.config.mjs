4f9f6e4769085415b6ad69269d22b92d18fde76b
xashadulpo <xashadulasif@gmail.com>
2024-10-12 21:30:51 +0600

    try 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  reactStrictMode:true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://*.pages.dev https://secure-mobile.walletconnect.com https://*.ngrok-free.app;"
+          }
+        ]
+      }
+    ];
+  },
 };
-
-export default nextConfig;
